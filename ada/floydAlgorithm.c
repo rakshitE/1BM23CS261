34fbaef4bfba3c45f98d0d029ab3112f06e8a33b
rakshitE <rakshits.cs23@bmsce.ac.in>
2025-05-26 22:34:35 +0530

    Create floydAlgorithm.c

---
+++ ada/floydAlgorithm.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+#define V 4
+#define INF 99999  // A large number representing "infinity"
+
+// Function to return minimum of two numbers
+int min(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+// Function to print the distance matrix
+void printMatrix(int dist[V][V]) {
+    printf("Shortest distances between every pair of vertices:\n");
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("%7s", "INF");
+            else
+                printf("%7d", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// Floyd-Warshall algorithm using min()
+void floydWarshall(int graph[V][V]) {
+    int dist[V][V];
+
+    // Initialize the distance matrix with graph values
+    for (int i = 0; i < V; i++)
+        for (int j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
+
+    // Update the shortest paths using each vertex as an intermediate
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                // Only add if there is no "infinity" involved
+                if (dist[i][k] != INF && dist[k][j] != INF) {
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+    }
+
+    printMatrix(dist);
+}
+
+int main() {
+    // Example graph represented as an adjacency matrix
+    int graph[V][V] = {
+        {0,   5,  INF, 10},
+        {INF, 0,   3, INF},
+        {INF, INF, 0,   1},
+        {INF, INF, INF, 0}
+    };
+
+    floydWarshall(graph);
+    return 0;
+}
